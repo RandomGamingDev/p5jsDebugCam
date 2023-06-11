a294c0bef17a7ea62fbd4c1ecaf1179e7099ba74
RandomGamingDev <83996185+RandomGamingDev@users.noreply.github.com>
2023-06-10 22:15:01 -0400

    Update debugCam.js

--- debugCam.js
+++ debugCam.js
@@ -8,34 +8,34 @@ class DebugCam {
   takeInput() {
     switch (keyCode) {
       case 87:
-        this.cam.move(0, 0, -moveSpeed);
+        this.cam.move(0, 0, -this.moveSpeed);
         break;
       case 83:
-        this.cam.move(0, 0, moveSpeed);
+        this.cam.move(0, 0, this.moveSpeed);
         break;
       case 65:
-        this.cam.move(-moveSpeed, 0, 0);
+        this.cam.move(-this.moveSpeed, 0, 0);
         break;
       case 68:
-        this.cam.move(moveSpeed, 0, 0);
+        this.cam.move(this.moveSpeed, 0, 0);
         break;
       case 32:
-        this.cam.move(0, -moveSpeed, 0);
+        this.cam.move(0, -this.moveSpeed, 0);
         break;
       case 16:
-        this.cam.move(0, moveSpeed, 0);
+        this.cam.move(0, this.moveSpeed, 0);
         break;
       case 38:
-        this.cam.tilt(-turnSpeed);
+        this.cam.tilt(-this.turnSpeed);
         break;
       case 40:
-        this.cam.tilt(turnSpeed);
+        this.cam.tilt(this.turnSpeed);
         break;
       case 37:
-        this.cam.pan(turnSpeed);
+        this.cam.pan(this.turnSpeed);
         break;
       case 39:
-        this.cam.pan(-turnSpeed);
+        this.cam.pan(-this.turnSpeed);
         break;
     }
   }
